bee50c3c22868476922467081fb06b8f9d4bbb4c
Jordan Hand <handjw1@student.op.ac.nz>
2022-06-22 19:16:00 +1200

    Finishing up for the day

--- Assets/js/filter.js
+++ Assets/js/filter.js
@@ -1,18 +1,18 @@
 filterSelection("all")
 function filterSelection(c) {
-  var x, i;
-  x = document.getElementsByClassName("filterDiv");
+  let x, i;
+  x = document.querySelectorAll(".filterDiv");
   if (c == "all") c = "";
-  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+  // Adds the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
   for (i = 0; i < x.length; i++) {
     RemoveClass(x[i], "show");
     if (x[i].className.indexOf(c) > -1) AddClass(x[i], "show");
   }
 }
 
-// Show filtered elements
+// Shows filtered elements
 function AddClass(element, name) {
-  var i, arr1, arr2;
+  let i, arr1, arr2;
   arr1 = element.className.split(" ");
   arr2 = name.split(" ");
   for (i = 0; i < arr2.length; i++) {
@@ -22,9 +22,9 @@ function AddClass(element, name) {
   }
 }
 
-// Hide elements that are not selected
+// Hides elements that are not selected
 function RemoveClass(element, name) {
-  var i, arr1, arr2;
+  let i, arr1, arr2;
   arr1 = element.className.split(" ");
   arr2 = name.split(" ");
   for (i = 0; i < arr2.length; i++) {
@@ -35,13 +35,15 @@ function RemoveClass(element, name) {
   element.className = arr1.join(" ");
 }
 
-// Add active class to the current control button (highlight it)
-var btnContainer = document.getElementById("myBtnContainer");
-var btns = btnContainer.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
+// Adds active class to the current control button (highlights it)
+let btnContainer = document.getElementById("myBtnContainer");
+let btns = document.querySelectorAll(".filterButton");
+for (let i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
+    let current = document.querySelectorAll("active");
     current[0].className = current[0].className.replace(" active", "");
     this.className += " active";
   });
 }
+
+
