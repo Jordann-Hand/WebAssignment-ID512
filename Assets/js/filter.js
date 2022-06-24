ca1098c817ab1f46f47d65788fc9b12e8857662a
Jordan Hand <handjw1@student.op.ac.nz>
2022-06-24 15:30:58 +1200

    Filter js working, just not going 100% but will be fine for now

--- Assets/js/filter.js
+++ Assets/js/filter.js
@@ -1,3 +1,4 @@
+
 filterSelection("all")
 function filterSelection(c) {
   let x, i;
@@ -5,8 +6,8 @@ function filterSelection(c) {
   if (c == "all") c = "";
   // Adds the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
   for (i = 0; i < x.length; i++) {
-    RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) AddClass(x[i], "show");
+    AddClass(x[i], "hide");
+    if (x[i].className.indexOf(c) > -1) RemoveClass(x[i], "hide");
   }
 }
 
@@ -16,9 +17,7 @@ function AddClass(element, name) {
   arr1 = element.className.split(" ");
   arr2 = name.split(" ");
   for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {
-      element.className += " " + arr2[i];
-    }
+    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
   }
 }
 
@@ -29,21 +28,22 @@ function RemoveClass(element, name) {
   arr2 = name.split(" ");
   for (i = 0; i < arr2.length; i++) {
     while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);
+      arr1.splice(arr1.indexOf(arr2[i]), 1);     
     }
   }
   element.className = arr1.join(" ");
-}
+} 
 
 // Adds active class to the current control button (highlights it)
-let btnContainer = document.getElementById("myBtnContainer");
+let btnContainer = document.getElementById("#myBtnContainer");
 let btns = document.querySelectorAll(".filterButton");
 for (let i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
-    let current = document.querySelectorAll("active");
+    let current = document.querySelectorAll(".active");
     current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
+    this.className +=" active";
   });
-}
+} 
+
 
 
