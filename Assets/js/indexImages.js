895b4b91e9794ef2f595ea1f99f1d7805b95b7ca
Jordan Hand <handjw1@student.op.ac.nz>
2022-06-27 21:41:48 +1200

    Index images fetch complete. Added styling and forced the p text below it

--- Assets/js/indexImages.js
+++ Assets/js/indexImages.js
@@ -2,28 +2,23 @@ const BASE_URL = "https://api.unsplash.com/";
 
 //Fetching from unsplash api
 fetch(`${BASE_URL}/search/photos?
-client_id=TbEAYJusOERXIWmLrfswwja4zPnxktEZYiVDtxvJpXY&query=tea-leaves`)
+client_id=TbEAYJusOERXIWmLrfswwja4zPnxktEZYiVDtxvJpXY&query=tea-leaves&per_page=5`)
 .then(response=> response.json())
 .then(data => {
     data["results"].forEach(r => {
         //Creating each individual photo and storying inside a div of class column
+        
         let column = document.createElement("div");
-        column.classList.add("column");
-        column.style.backgroundImage = `url(${r["urls"]["regular"]})`;
+        column.classList.add("column3");
+        column.style.backgroundImage = `url(${r["urls"]["small"]})`;
 
         //Alternative description of each image to appear
-        let product_title = 
-        document.createElement("div");
+        let product_title = document.createElement("p");
         product_title.innerHTML = r["alt_description"];
-        product_title.classList.add("product_title");
-        //append product title inside of column
-        column.append(product_title);       
+        column.append(product_title);
 
         //append column inside of row class
-        document.querySelector(".row").append(column); 
-        
+        document.querySelector(".middleImages").append(column);
         
     });
 });
-
- 
